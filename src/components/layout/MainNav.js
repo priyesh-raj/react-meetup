135f4fcbee331f36ec3acfd128c97cb1953efefa
priyesh.raj9 <priyesh.raj@wissen.com>
2022-07-28 14:05:01 +0530

    favourites badge on main navigation

--- src/components/layout/MainNav.js
+++ src/components/layout/MainNav.js
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { FavouritesContext } from "../../store/favourites-context";
 
 import classes from "./MainNav.module.css";
 
-export class MainNav extends React.Component {
-  render() {
-    return (
-      <header className={classes.header}>
-        <div className={classes.logo}>React Meetups</div>
-        <nav>
-          <ul>
-            <li>
-              <Link to={"/"}>All Meetups</Link>
-            </li>
-            <li>
-              <Link to={"/favourites"}>Favourites</Link>
-            </li>
-            <li>
-              <Link to={"/new-meetup"}>New Meetups</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
+export function MainNav() {
+  const favCtx = useContext(FavouritesContext);
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/"}>All Meetups</Link>
+          </li>
+          <li>
+            <Link to={"/favourites"}>
+              Favourites
+              {favCtx.totalFavourites ? `| ${favCtx.totalFavourites}` : ""}
+            </Link>
+          </li>
+          <li>
+            <Link to={"/new-meetup"}>New Meetups</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
+
+// export class MainNav extends React.Component {
+//   render() {
+//     return (
+//       <header className={classes.header}>
+//         <div className={classes.logo}>React Meetups</div>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to={"/"}>All Meetups</Link>
+//             </li>
+//             <li>
+//               <Link to={"/favourites"}>Favourites |</Link>
+//             </li>
+//             <li>
+//               <Link to={"/new-meetup"}>New Meetups</Link>
+//             </li>
+//           </ul>
+//         </nav>
+//       </header>
+//     );
+//   }
+// }
